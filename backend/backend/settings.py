fa832d74a27eed327bc89b82062549a400a3a8b8
Tuan <20120395@student.hcmus.edu.vn>
2023-05-26 07:33:07 +0700

    Setting Mysql database connection in Django

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import pymysql
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -75,11 +76,18 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hotel',
+        'USER': 'nhom9',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
+# Fake version of pymysql
+pymysql.version_info = (1, 4, 3, 'final', 0)
+pymysql.install_as_MySQLdb()
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
